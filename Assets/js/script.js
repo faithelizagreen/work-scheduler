09e4e4e4a829801768e6362858d13612f566dcf7
faithelizagreen <faithgreen@knights.ucf.edu>
2021-09-30 15:18:40 -0500

    Loaded local storage onto the page and created function to track time.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,3 +14,44 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
 })
+
+ // load saved data from local storage
+ $("#hour8 .description").val(localStorage.getItem("hour8"));
+ $("#hour9 .description").val(localStorage.getItem("hour9"));
+ $("#hour10 .description").val(localStorage.getItem("hour10"));
+ $("#hour11 .description").val(localStorage.getItem("hour11"));
+ $("#hour12 .description").val(localStorage.getItem("hour12"));
+ $("#hour13 .description").val(localStorage.getItem("hour13"));
+ $("#hour14 .description").val(localStorage.getItem("hour14"));
+ $("#hour15 .description").val(localStorage.getItem("hour15"));
+ $("#hour16 .description").val(localStorage.getItem("hour16"));
+ $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+ function timeTracker() {
+    //get current number of hours.
+    let timeNow = moment().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        let blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+timeTracker()
